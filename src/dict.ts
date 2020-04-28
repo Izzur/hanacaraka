93bf6490003ab14737dd520de4e23899198bfee3
Izzur Zuhri <izzur.zuhri@gmail.com>
2020-04-28 22:21:54 +0700

    Add letter from A984 to A9A8 to dict

--- src/dict.ts
+++ src/dict.ts
@@ -1,5 +1,190 @@
 export default [
   {
+    value: "\uA984",
+    name: "JAVANESE LETTER A",
+    transc: "a",
+  },
+  {
+    value: "\uA985",
+    name: "JAVANESE LETTER I KAWI",
+    transc: "i",
+  },
+  {
+    value: "\uA986",
+    name: "JAVANESE LETTER I",
+    transc: "i",
+  },
+  {
+    value: "\uA987",
+    name: "JAVANESE LETTER II",
+    transc: "ii",
+  },
+  {
+    value: "\uA988",
+    name: "JAVANESE LETTER U",
+    transc: "u",
+  },
+  {
+    value: "\uA989",
+    name: "JAVANESE LETTER PA CEREK",
+    transc: "rĕ",
+  },
+  {
+    value: "\uA98A",
+    name: "JAVANESE LETTER NGA LELET",
+    transc: "lĕ",
+  },
+  {
+    value: "\uA98B",
+    name: "JAVANESE LETTER NGA LELET RASWADI",
+    transc: "leu",
+  },
+  {
+    value: "\uA98C",
+    name: "JAVANESE LETTER E",
+    transc: "e",
+  },
+  {
+    value: "\uA98D",
+    name: "JAVANESE LETTER AI",
+    transc: "ai",
+  },
+  {
+    value: "\uA98E",
+    name: "JAVANESE LETTER O",
+    transc: "o",
+  },
+  {
+    value: "\uA98F",
+    name: "JAVANESE LETTER KA",
+    transc: "ka",
+  },
+  {
+    value: "\uA990",
+    name: "JAVANESE LETTER KA SASAK",
+    transc: "qa",
+  },
+  {
+    value: "\uA991",
+    name: "JAVANESE LETTER KA MURDA",
+    transc: "kha",
+  },
+  {
+    value: "\uA992",
+    name: "JAVANESE LETTER GA",
+    transc: "ga",
+  },
+  {
+    value: "\uA993",
+    name: "JAVANESE LETTER GA MURDA",
+    transc: "gha",
+  },
+  {
+    value: "\uA994",
+    name: "JAVANESE LETTER NGA",
+    transc: "nga",
+  },
+  {
+    value: "\uA995",
+    name: "JAVANESE LETTER CA",
+    transc: "ca",
+  },
+  {
+    value: "\uA996",
+    name: "JAVANESE LETTER CA MURDA",
+    transc: "cha",
+  },
+  {
+    value: "\uA997",
+    name: "JAVANESE LETTER JA",
+    transc: "ja",
+  },
+  {
+    value: "\uA998",
+    name: "JAVANESE LETTER NYA MURDA",
+    transc: "jnya",
+  },
+  {
+    value: "\uA999",
+    name: "JAVANESE LETTER JA MAHAPRANA",
+    transc: "jha",
+  },
+  {
+    value: "\uA99A",
+    name: "JAVANESE LETTER NYA",
+    transc: "nya",
+  },
+  {
+    value: "\uA99B",
+    name: "JAVANESE LETTER TTA",
+    transc: "tta",
+  },
+  {
+    value: "\uA99C",
+    name: "JAVANESE LETTER TTA MAHAPRANA",
+    transc: "ttha",
+  },
+  {
+    value: "\uA99D",
+    name: "JAVANESE LETTER DDA",
+    transc: "dda",
+  },
+  {
+    value: "\uA99E",
+    name: "JAVANESE LETTER DDA MAHAPRANA",
+    transc: "ddha",
+  },
+  {
+    value: "\uA99F",
+    name: "JAVANESE LETTER NA MURDA",
+    transc: "nna",
+  },
+  {
+    value: "\uA9A0",
+    name: "JAVANESE LETTER TA",
+    transc: "ta",
+  },
+  {
+    value: "\uA9A1",
+    name: "JAVANESE LETTER TA MURDA",
+    transc: "tha",
+  },
+  {
+    value: "\uA9A2",
+    name: "JAVANESE LETTER DA",
+    transc: "da",
+  },
+  {
+    value: "\uA9A3",
+    name: "JAVANESE LETTER DA MAHAPRANA",
+    transc: "dha",
+  },
+  {
+    value: "\uA9A4",
+    name: "JAVANESE LETTER NA",
+    transc: "na",
+  },
+  {
+    value: "\uA9A5",
+    name: "JAVANESE LETTER PA",
+    transc: "pa",
+  },
+  {
+    value: "\uA9A6",
+    name: "JAVANESE LETTER PA MURDA",
+    transc: "pha",
+  },
+  {
+    value: "\uA9A7",
+    name: "JAVANESE LETTER BA",
+    transc: "ba",
+  },
+  {
+    value: "\uA9A8",
+    name: "JAVANESE LETTER BA MURDA",
+    transc: "bha",
+  },
+  {
     value: "\uA9A9",
     name: "JAVANESE LETTER MA",
     transc: "ma",
