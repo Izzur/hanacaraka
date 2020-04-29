bc82e293ebe2cf6cd10cb21927af02002521a49a
Izzur Zuhri <izzur.zuhri@gmail.com>
2020-04-29 23:12:32 +0700

    Add empty encode function

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
 import dict from "./dict";
 
-export function encode(params: String) {
-  toJavanese();
-  return params;
-}
-
-export function decode(params: String) {
-  const output = params.split("").map(fromJavanese);
+/**
+ * Change javanese script into corresponding latin character
+ * @param script
+ */
+export function decode(script: string) {
+  const output = script.split("").map(fromJavanese);
   return output.join("").toUpperCase();
 }
 
-function toJavanese() {}
+/**
+ * Change latin character into corresponding javanese script
+ * @param text
+ */
+export function encode(text: string) {
+  const syllables = splitSyllable(text);
+  const output = syllables.map(toJavanese);
+  return output;
+}
 
-function fromJavanese(letter) {
+function fromJavanese(letter: string) {
   const index = dict.map((x) => x.value).indexOf(letter);
   return dict[index].transc;
+}
+
+function toJavanese(letter: string) {
+  return letter;
+}
+
+function splitSyllable(letter: string) {
+  return letter.split("");
 }
