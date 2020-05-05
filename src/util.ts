1ee50b844970dbfbc4c45a665f77f47dc569ecfc
Izzur Zuhri <izzur.zuhri@gmail.com>
2020-05-05 22:35:02 +0700

    Add handle for vowel

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,4 @@
-import { letters } from "./dict";
+import { letters, vowels } from "./dict";
 
 export function fromJavanese(syllable: string) {
   const index = letters.map((x) => x.value).indexOf(syllable);
@@ -6,14 +6,41 @@ export function fromJavanese(syllable: string) {
 }
 
 export function toJavanese(syllable: string) {
-  const combined: string[] = [];
-  const index = letters.map((x) => x.transc).indexOf(syllable);
-  if (index < 0) return "";
-  combined.push(letters[index].value);
-  return combined.join("");
+  const vowelRegex = /[aiueoĕ]/g;
+  const vowel = syllable.match(vowelRegex);
+  if (!vowel) return "";
+  const base = getSymbolFrom(letters, syllable.replace(vowelRegex, "a"));
+  return vowelify(base, vowel[0]);
 }
 
 export function splitSyllable(word: string) {
   const regex = /(?<=[bcdfghjklmnpqrstvwxyz][aiueo])/;
   return word.split(regex);
+}
+
+export function vowelify(syllable: string, vowel: string) {
+  const combined: string[] = [syllable];
+  switch (vowel) {
+    case "i":
+    case "u":
+    case "ĕ":
+      combined.push(getSymbolFrom(vowels, vowel));
+      break;
+    case "e":
+      combined.unshift(getSymbolFrom(vowels, vowel));
+      break;
+    case "o":
+      combined.unshift(getSymbolFrom(vowels, "e"));
+      combined.push(getSymbolFrom(vowels, "aa"));
+      break;
+    default:
+      break;
+  }
+  return combined.join("");
+}
+
+export function getSymbolFrom(dictionary: any[], symbol: string) {
+  const index = dictionary.map((x) => x.transc).indexOf(symbol);
+  if (index < 0) return "";
+  return dictionary[index].value;
 }
