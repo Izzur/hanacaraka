76a3c292e8a5a40b4be7d10eec637a86fe30a3cc
Izzur Zuhri <izzur.zuhri@gmail.com>
2020-05-20 21:48:55 +0700

    Add test case for digit number

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,9 @@ import { encode, decode } from "../src";
 
 test("Encode latin to hanacaraka", () => {
   expect(encode("HANACARAKA")).toBe("\uA9B2\uA9A4\uA995\uA9AB\uA98F");
+  expect(encode("1234567890")).toBe(
+    "\uA9D1\uA9D2\uA9D3\uA9D4\uA9D5\uA9D6\uA9D7\uA9D8\uA9D9\uA9D0"
+  );
 });
 
 test("Decode hanacaraka to latin", () => {
